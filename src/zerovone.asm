5dac54c18083502fe95c3332543c32c90008e75b
Jason Kostempski <jasonkostempski@gmail.com>
2020-04-07 18:31:58 -0400

    Some drawing and moving

--- src/zerovone.asm
+++ src/zerovone.asm
@@ -3,50 +3,97 @@
     include "vcs.h"
     include "macro.h"
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Variables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
     seg.u Variables
     org $80
 
+; X positions
 P0X .byte
 P1X .byte
 M0X .byte
 M1X .byte
 BLX .byte
 
+; Y positions
 P0Y .byte
 P1Y .byte
 M0Y .byte
 M1Y .byte
 BLY .byte
 
-PH equ 8
+; X forces
+P0FX .byte
+P1FX .byte
+BLFX .byte
+
+; Y forces
+P0FY .byte
+P1FY .byte
+BLFY .byte
+
+; Sprite Pointers
+P0SPR .word
+P1SPR .word
+
+; Sprite offsets
+P0SPROFF .byte
+P1SPROFF .byte
+
+; Constants (prepend # when used)
+PH equ 3
+BALLHEIGHT equ 5
+GRAVITY equ 1
+COLORP0 equ $36
+COLORP1 equ $98
+COLORBL equ $06
 
     seg Code
     org $f000
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Initialize
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 Start
     CLEAN_START
 
-    lda #10
-    sta P0X
-    sta P0Y
-    sta M0X
-    sta M0Y
+    jsr StartPositions
 
-    lda #100
-    sta P1X
-    sta P1Y
-    sta M1X
-    sta M1Y
+    lda #COLORP0
+    sta COLUP0
+    lda #COLORP1
+    sta COLUP1
+    lda #COLORBL
+    sta COLUPF
+
+    lda #2
+    sta ENABL
+
+    lda #0
+    tay
+
+    lda (P0SPR),y
+    sta GRP0
+
+    lda (P1SPR),y
+    sta GRP1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 Frame
     VERTICAL_SYNC
 
-; Vertical Blank
+; Vertical Blank timer setup
     lda #42 ;(((36 * 76) + 13) / 64) = 42.9531
     sta WSYNC
     sta TIM64T
 
-; Set X for all 5 objects
+; Set X on all objects
     ldx #4
 XLoop
     lda P0X,x
@@ -57,48 +104,78 @@ XLoop
     sta WSYNC
     sta HMOVE
 
+; Vertical Blank Wait
 WaitVBlank
     lda INTIM
     bne WaitVBlank
-; End Vertical Blank
 
-; Visible
-
-    lda #0
+; Turn off VBLANK
+; A should already be zero? If somehow not, then lda #0 here
     sta VBLANK
 
+; Visible timer setup
     lda #229 ;(((192 * 76) + 13) / 64) = 228.203125
     sta WSYNC
     sta TIM64T
 
-    ; Draw logic
+; Draw logic
 
+; Visible wait
 WaitVisible
     lda INTIM
     bne WaitVisible
 
-; End Visible
-
-; Overscan
-
-    lda #%10000010
+; VBLANK on
+    lda #2
     sta VBLANK
 
+; Overscan timer setup
     lda #36 ;(((30 * 76) + 13) / 64) = 35.828125
     sta WSYNC
     sta TIM64T
 
-    ; Overscan logic
+; Overscan logic
+    jsr ReadJoysticks
 
+; Overscan wait
 WaitOverscan
     lda INTIM
     bne WaitOverscan
 
-; End Overscan
-
+; Next frame
     jmp Frame
 
-; Subroutines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Subroutines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+StartPositions
+    lda #5
+    sta P0X
+
+    lda #79
+    sta BLX
+
+    lda #147
+    sta P1X
+
+    lda #0
+    sta P0Y
+    sta P1Y
+    sta BLY
+
+    lda #8
+    sta REFP1
+
+    lda #<PGFX0
+    sta P0SPR
+    sta P1SPR
+
+    lda #>PGFX0
+    sta P0SPR+1
+    sta P1SPR+1
+
+    rts
 
 SetX
     sta WSYNC
@@ -116,8 +193,67 @@ SetXDivide
     sta HMP0,x
     rts
 
-; Data
+ReadJoysticks
+    lda #$10
+    bit SWCHA
+    bne DownP0
+    dec P0Y
+    jmp LeftP0
+DownP0
+    lda #$20
+    bit SWCHA
+    bne LeftP0
+    inc P0Y
+LeftP0
+    lda #$40
+    bit SWCHA
+    bne RightP0
+    dec P0X
+    jmp UpP1
+RightP0
+    lda #$80
+    bit SWCHA
+    bne UpP1
+    inc P0X
+UpP1
+    lda #$01
+    bit SWCHA
+    bne DownP1
+    dec P1Y
+    jmp LeftP1
+DownP1
+    lda #$02
+    bit SWCHA
+    bne LeftP1
+    inc P1Y
+LeftP1
+    lda #$04
+    bit SWCHA
+    bne RightP1
+    dec P1X
+    jmp EndJoy
+RightP1
+    lda #$08
+    bit SWCHA
+    bne EndJoy
+    inc P1X
+EndJoy
+    rts
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    align $100
+
+PGFX0
+    .byte #%11000000
+    .byte #%11111110
+    .byte #%01000100
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Finalize
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $fffc
 
     .word Start
