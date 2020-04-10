391ec936957ad2fe06651fcb1024f08f5508b63e
Jason Kostempski <jasonkostempski@gmail.com>
2020-04-10 16:21:32 -0400

    Players drawing and moving with boundaries

--- src/zerovone.asm
+++ src/zerovone.asm
@@ -3,159 +3,174 @@
     include "vcs.h"
     include "macro.h"
 
-;Battle at $0280
-;$1B vs $1C
-;0v1
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Variables
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
     seg.u Variables
     org $80
 
-; X positions
-P0X .byte
-P1X .byte
-M0X .byte
-M1X .byte
-BLX .byte
+XP0 .byte
+XP1 .byte
+XM0 .byte
+XM1 .byte
+XBL .byte
 
-; Y positions
-P0Y .byte
-P1Y .byte
-M0Y .byte
-M1Y .byte
-BLY .byte
+YP0 .byte
+YP1 .byte
+YM0 .byte
+YM1 .byte
+YBL .byte
 
-; X forces
-P0FX .byte
-P1FX .byte
-BLFX .byte
+XVP0 .byte
+XVP1 .byte
+XVBL .byte
 
-; Y forces
-P0FY .byte
-P1FY .byte
-BLFY .byte
+YVP0 .byte
+YVP1 .byte
+YVBL .byte
 
-; Sprite Pointers
-P0SPR .word
-P1SPR .word
+LineColorP0 .byte
+LineColorP1 .byte
 
-; Constants
-PH equ 4
-BALLHEIGHT equ 5
-GRAVITY equ 1
-COLORP0 equ $36
-COLORP1 equ $98
-COLORBL equ $06
-VBLANKTIM64T equ 42 ;((((37 - 1) * 76) + 13) / 64) = 42.9531
+LineCountP0 .byte
+LineCountP1 .byte
+LineCountM0 .byte
+LineCountM1 .byte
+LineCountBL .byte
+
+PtrSpriteP0 .word
+PtrSpriteP1 .word
+PtrColorP0 .word
+PtrColorP1 .word
+
+PlayerHeight equ 9
+BallHeight equ 9
 
     seg Code
     org $f000
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Initialize
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 Start
     CLEAN_START
 
-    lda #COLORP0
-    sta COLUP0
-    lda #COLORP1
-    sta COLUP1
-    lda #COLORBL
-    sta COLUPF
-
+;; Delay P0 and BL
     lda #1
     sta VDELP0
     sta VDELBL
 
+;; Set BL size 4 and PF to Score mode and Reflect
     lda #%00100011
     sta CTRLPF
 
+;; Init pointers
+    lda #<P0Colors
+    sta PtrColorP0
+    lda #>P0Colors
+    sta PtrColorP0+1
+
+    lda #<P1Colors
+    sta PtrColorP1
+    lda #>P1Colors
+    sta PtrColorP1+1
+
+    lda #<PlayerBitmap
+    sta PtrSpriteP0
+    sta PtrSpriteP1
+
+    lda #>PlayerBitmap
+    sta PtrSpriteP0+1
+    sta PtrSpriteP1+1
+
+;; Set initial positions
     jsr StartPositions
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Frame
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;; Frame
 Frame
     VERTICAL_SYNC
 
-; Vertical Blank timer setup
-    lda #VBLANKTIM64T
+;; Vertical Blank timer setup
+;; (((36 * 76) + 13) / 64) = 42.9531
+    lda #42
     sta WSYNC
     sta TIM64T
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Set X on all objects
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set X on all objects
     ldx #4
-XLoop
-    lda P0X,x
+SetXLoop
+    lda XP0,x
     jsr SetX
     dex
-    bpl XLoop
+    bpl SetXLoop
     sta WSYNC
     sta HMOVE
 
-; Vertical Blank Wait
+; Init Y counters to object Y coordinates
+    ldx #4
+YCounterLoop
+    lda YP0,x
+    sta LineCountP0,x
+    dex
+    bpl YCounterLoop
+
+;; Vertical Blank Wait
 WaitVBlank
     lda INTIM
     bne WaitVBlank
-    sta VBLANK
+    sta VBLANK ; Turn off VBLANK. A is already 0.
 
-;; Visible 2-line kernel
-    ldx #192
+;; Visible
+    ldx #96
 KernelLoop
-; Ball
-    txa
-    sec
-    sbc BLY
-    cmp #BALLHEIGHT
-    bcc .DrawBall
-    lda #0
-    jmp .SetBall
-.DrawBall
-    lda #2
-.SetBall
+
+;; Ball
+    lda #BallHeight
     sta ENABL
-; P0
-    txa
     sec
-    sbc P0Y
-    cmp #PH
-    bcc .DrawP0
+    isb LineCountBL
+    bcc .NoBall
+    lda #2
+    sta ENABL
+.NoBall
+
+;; P0
+    lda #PlayerHeight
+    sec
+    isb LineCountP0
+    bcs .DrawP0
     lda #0
 .DrawP0
     tay
-    lda (P0SPR),y
+    lda (PtrColorP0),y
+    sta LineColorP0
+    lda (PtrSpriteP0),y
     sta GRP0
+
 ; P1
-    txa
+    lda #PlayerHeight
     sec
-    sbc P1Y
-    cmp #PH
-    bcc .DrawP1
+    isb LineCountP1
+    bcs .DrawP1
     lda #0
 .DrawP1
     tay
-    lda (P1SPR),y)
+    lda (PtrColorP1),y
+    sta LineColorP1
+    lda (PtrSpriteP1),y
+    tay
+
+    lda LineColorP0
     sta WSYNC
-    sta GRP1
+    sta COLUP0
+    lda LineColorP1
+    sta COLUP1
+    sty GRP1
+
     dex
     bne KernelLoop
 
-;; End Visible
-
-; VBLANK on
+;; Overscan
     lda #2
     sta VBLANK
 
-; Overscan timer setup
-    lda #36 ;(((30 * 76) + 13) / 64) = 35.828125
+;; Overscan timer setup
+;; (((30 * 76) + 13) / 64) = 35.828125
+    lda #36
     sta WSYNC
     sta TIM64T
 
@@ -177,34 +192,26 @@ WaitOverscan
 
 StartPositions
     lda #5
-    sta P0X
-
+    sta XP0
     lda #79
-    sta BLX
-
+    sta XBL
     lda #147
-    sta P1X
+    sta XP1
 
-    lda #PH
-    sta P0Y
-    sta P1Y
-
-    lda #BALLHEIGHT
-    sta BLY
+    lda #242;#PlayerHeight
+    sta YP0
+    sta YP1
+    lda #242;#BallHeight
+    sta YBL
 
     lda #8
     sta REFP1
 
-    lda #<PGFX0
-    sta P0SPR
-    sta P1SPR
-
-    lda #>PGFX0
-    sta P0SPR+1
-    sta P1SPR+1
-
     rts
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set X Coordinate (A) of object offset (X) from P0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 SetX
     sta WSYNC
     bit 0
@@ -222,77 +229,171 @@ SetXDivide
     rts
 
 ReadJoysticks
+;; Y P0
+    ldx YP0
+    ldy #0
+
+.UpP0
     lda #$10
     bit SWCHA
-    bne DownP0
-    inc P0FY
-    jmp LeftP0
-DownP0
+    bne .DownP0
+    cpx #254
+    bcs .ApplyYVP0
+    ldy #$01
+    jmp .ApplyYVP0
+
+.DownP0
     lda #$20
     bit SWCHA
-    bne LeftP0
-    dec P0FY
-LeftP0
+    bne .ApplyYVP0
+    cpx #175
+    bcc .ApplyYVP0
+    ldy #$FF
+
+.ApplyYVP0
+    sty YVP0
+
+;; X P0
+    ldx XP0
+    ldy #0
+
+.LeftP0
     lda #$40
     bit SWCHA
-    bne RightP0
-    dec P0FX
-    jmp UpP1
-RightP0
+    bne .RightP0
+    cpx #1
+    bcc .ApplyXVP0
+    ldy #$FF
+    jmp .ApplyXVP0
+.RightP0
     lda #$80
     bit SWCHA
-    bne UpP1
-    inc P0FX
-UpP1
+    bne .ApplyXVP0
+    cpx #152
+    bcs .ApplyXVP0
+    ldy #$01
+
+.ApplyXVP0
+    sty XVP0
+
+;; Y P1
+    ldx YP1
+    ldy #0
+
+.UpP1
     lda #$01
     bit SWCHA
-    bne DownP1
-    inc P1FY
-    jmp LeftP1
-DownP1
+    bne .DownP1
+    cpx #254
+    bcs .ApplyYVP1
+    ldy #$01
+    jmp .ApplyYVP1
+
+.DownP1
     lda #$02
     bit SWCHA
-    bne LeftP1
-    dec P1FY
-LeftP1
+    bne .ApplyYVP1
+    cpx #175
+    bcc .ApplyYVP1
+    ldy #$FF
+
+.ApplyYVP1
+    sty YVP1
+
+;; X P1
+    ldx XP1
+    ldy #0
+
+.LeftP1
     lda #$04
     bit SWCHA
-    bne RightP1
-    dec P1FX
-    jmp EndJoy
-RightP1
+    bne .RightP1
+    cpx #1
+    bcc .ApplyXVP1
+    ldy #$FF
+    jmp .ApplyXVP1
+.RightP1
     lda #$08
     bit SWCHA
-    bne EndJoy
-    inc P1FX
-EndJoy
+    bne .ApplyXVP1
+    cpx #152
+    bcs .ApplyXVP1
+    ldy #$01
+
+.ApplyXVP1
+    sty XVP1
+
     rts
 
 ApplyForces
-    lda P0X
+; P0
+    lda XP0
     clc
-    adc P0FX
-    sta P0X
+    adc XVP0
+    sta XP0
 
-    lda P0Y
+    lda YP0
     clc
-    adc P0FY
-    sta P0Y
+    adc YVP0
+    sta YP0
+; P1
+    lda XP1
+    clc
+    adc XVP1
+    sta XP1
+
+    lda YP1
+    clc
+    adc YVP1
+    sta YP1
+; BL
+    lda XBL
+    clc
+    adc XVBL
+    sta XBL
+
+    lda YBL
+    clc
+    adc YVBL
+    sta YBL
     rts
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; DATA
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-PGFX0
-    .byte #%00000000
-    .byte #%01000100
-    .byte #%11111110
-    .byte #%11000000
+PlayerBitmap
+    .byte #0
+    .byte #%00011000
+    .byte #%00100100
+    .byte #%01000010
+    .byte #%01011010
+    .byte #%01111110
+    .byte #%01000010
+    .byte #%00100100
+    .byte #%00011000
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+P0Colors
+    .byte #0
+    .byte #$06;
+    .byte #$06;
+    .byte #$06;
+    .byte #$38;
+    .byte #$38;
+    .byte #$06;
+    .byte #$06;
+    .byte #$06;
+
+P1Colors
+    .byte #0
+    .byte #$06;
+    .byte #$06;
+    .byte #$06;
+    .byte #$98;
+    .byte #$98;
+    .byte #$06;
+    .byte #$06;
+    .byte #$06;
+
 ;; Finalize
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $fffc
 
     .word Start
